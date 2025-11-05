22564869fdbaf42c8d5ac45e7723bdec87ae8370
thomasfavalier@gmail.com <thomasfavalier@gmail.com>
2025-11-05 09:46:37 +0000

    blabla

--- models/mart/finance_days.sql
+++ models/mart/finance_days.sql
@@ -2,7 +2,7 @@ select
 date_date,
 COUNT(orders_id) AS nb_of_transactions,
 ROUND(SUM(revenue),2) AS total_revenue,
-ROUND(AVG(revenue),2) AS average_basket,
+AVG((SUM(revenue))/(COUNT(DISTINCT(orders_id))),2) AS average_basket,
 ROUND(SUM(operational_margin),2) AS operational_margin,
 ROUND(SUM(purchase_cost),2) AS total_purchase_cost,
 ROUND(SUM(shipping_fee),2) AS total_shipping_fee,
